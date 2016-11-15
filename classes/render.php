ad5e531300b092c26907c26276dc64769b736204
Josh Pollock <jpollock412@gmail.com>
2016-11-15 10:58:30 -0500

    Fix indents

--- classes/render.php
+++ classes/render.php
@@ -25,13 +25,14 @@ class  CF_Translate_Render extends CF_Translate_Filter{
 	 *
 	 * @return mixed
 	 */
-    public function translate( $field, $form ){
-        if( $this->form[ 'ID' ] == $form[ 'ID' ] ){
-            $field = $this->translate_field( $field );
-        }
-        return $field;
+	public function translate( $field, $form ){
+		if( $this->form[ 'ID' ] == $form[ 'ID' ] ){
+		    $field = $this->translate_field( $field );
+		}
+		
+		return $field;
 
-    }
+	}
 
 	/**
 	 * If possible, translate a field
@@ -42,39 +43,36 @@ class  CF_Translate_Render extends CF_Translate_Filter{
 	 *
 	 * @return array
 	 */
-    protected function translate_field( $field ){
-        $field_object  = $this->form->get_translator()->get_field( $this->args[ 'language' ], $field[ 'ID' ] );
-        /**  @var CF_Translate_Field $field_object */
-        if( is_object( $field_object ) ){
-            foreach( $field_object->get_field_names() as $key ){
+	protected function translate_field( $field ){
+		$field_object  = $this->form->get_translator()->get_field( $this->args[ 'language' ], $field[ 'ID' ] );
+		/**  @var CF_Translate_Field $field_object */
+		if( is_object( $field_object ) ){
+		    foreach( $field_object->get_field_names() as $key ){
 
-                if( 'ID' != $key  ){
-		            $value = $field_object->$key;
-		            if( ! empty( $value )  && isset( $field[ $key ] )  && $value != $field[ $key ] ) {
-			            $field[ $key ] = $field_object->$key;
-		            }
+			if( 'ID' != $key  ){
+				    $value = $field_object->$key;
+				    if( ! empty( $value )  && isset( $field[ $key ] )  && $value != $field[ $key ] ) {
+					    $field[ $key ] = $field_object->$key;
+				    }
 
-                }
-            }
+			}
+		    }
 
-        }
+		}
 
-        return $field;
+		return $field;
 
-    }
+	}
+	/**
+	* Add field translation hook if needed
+	*
+	* @since 0.1.0
+	*/
+	protected function add_hook (){
+		if ( $this->form->get_translator()->has_language( $this->args[ 'language'] )) {
+				parent::add_hook();
+		}
 
-
-
-    /**
-     * Add field translation hook if needed
-     *
-     * @since 0.1.0
-     */
-    protected function add_hook (){
-        if ( $this->form->get_translator()->has_language( $this->args[ 'language'] )) {
-			parent::add_hook();
-        }
-
-    }
+	}
 
 }
