58e4726a6f67756e82f8796a0eee586042569683
josh <josh@calderawp.com>
2017-11-04 18:57:52 -0400

    externalized app state setting https://gitlab.com/caldera-labs/vue/app-state

--- client/plugins/state/index.js
+++ client/plugins/state/index.js
@@ -1,49 +1,3 @@
-import  {
-	StateFactory,
-	StateFactoryReadOnly,
-	StateFactoryDispatching
-} from './factories';
-
-/**
- * Container for factories that are created as need
- *
- * @type {{twoWay: {}, dispatching: {}, oneWay: {}}}
- */
-let factories = {
-	twoWay: {},
-	dispatching: {},
-	oneWay: {},
-
-};
-
-/**
- * Factory for creating lazy-loading state factories
- *
- * @param type {String} Type of factory
- * @param factory {Function} Callback function to create state factories with.
- * @returns {function(*=)}
- */
-const factoryFactory = ( type, factory ) => {
-	return ( prop ) => {
-		if ( ! factories[ type ].hasOwnProperty( prop ) ) {
-			factories[ type ][prop] = new factory(prop);
-		}
-		return factories[ type ][prop]
-	};
-};
-
-/** Create Factories **/
-const twoWayFactory = factoryFactory( 'twoWay', StateFactory );
-const oneWayFactory = factoryFactory( 'oneWay', StateFactoryReadOnly );
-const dispatchingFactory = factoryFactory( 'dispatching', StateFactoryDispatching );
-
-/**
- * The computed properties to be injected into all components
- *
- * @type {{}}
- */
-let computed = {};
-
 const stateComputes = {
 	twoWay: [
 		'foo',
@@ -66,32 +20,7 @@ const stateComputes = {
 	]
 };
 
-/** Inject two-way bindings **/
-stateComputes.twoWay.forEach( (key,i) => {
-	computed[key] = twoWayFactory( key, key );
-});
+import { createStatePlugin } from "vue-app-state";
 
-/** Inject one-way bindings **/
-stateComputes.oneWay.forEach( (key,i) => {
-	computed[key] = oneWayFactory( key, key );
-});
-
-/** Inject two-way, asynchronous bindings **/
-stateComputes.dispatching.forEach( (key,i) => {
-	computed[key] = dispatchingFactory( key, key );
-});
-
-/**
- * Create plugin
- *
- * @type {{install: (function(*, *): void)}}
- */
-const statePlugin = {
-	install(Vue, options) {
-		Vue.mixin({
-			computed: computed
-		});
-	}
-};
-
+const statePlugin = createStatePlugin(stateComputes);
 export default statePlugin;
